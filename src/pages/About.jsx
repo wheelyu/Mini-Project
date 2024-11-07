e09cfc00c7e7e7dcbfb4ab8336fe1926dc59473a
wheelyu <harun.abdulkarim17@gmail.com>
2024-11-07 11:39:14 +0700

    install template

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-3xl font-bold underline">About page</h1>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-3xl font-bold underline">Home page</h1>
+    </div>
+  );
+}
