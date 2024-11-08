a481a4ffd2e2ec45353771f4d575f48ed861f741
wheelyu <harun.abdulkarim17@gmail.com>
2024-11-08 19:10:16 +0700

    hapus

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -131,7 +131,6 @@ const SelectForm = () => {
 
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
           className="w-full bg-yellow-600 text-white font-semibold py-2 px-4 rounded hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500"
         >
           Submit
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 p-4">
     <nav className="bg-yellow-600 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <a href="/" className="text-white font-bold text-xl">LacakUV</a>
