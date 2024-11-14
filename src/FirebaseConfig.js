e2f6db2d4277359201df81760006fa89b20c934c
wheelyu <harun.abdulkarim17@gmail.com>
2024-11-14 22:57:37 +0700

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -9,15 +9,16 @@ import { getDatabase, ref as dbRef, set, push } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey:"AIzaSyDlgaxCtMxL8jgwSVmQsOZueJfyOX09T0I",
-    authDomain: "indexuv-2bbb8.firebaseapp.com",
-    databaseURL: "https://indexuv-2bbb8-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "indexuv-2bbb8",
-    storageBucket: "indexuv-2bbb8.firebasestorage.app",
-    messagingSenderId: "496294560268",
-    appId: "1:496294560268:web:2b9c42acccfedbd6bb6429",
-    measurementId: "G-Z9H87CNE5S"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 
