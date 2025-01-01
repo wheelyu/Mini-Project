2c571a968fd5a6daab9f4e3c48bc5cb44f2c9b37
wheelyu <harun.abdulkarim17@gmail.com>
2025-01-01 09:04:21 +0700

    Update Landing.jsx

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -5,21 +5,21 @@ const LandingPage = () => {
     return (
         <section className="flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen bg-white dark:bg-[#121212] py-28 px-4 md:py-12 md:px-8 lg:py-20 lg:px-16 xl:px-40">
             {/* Bagian Kanan - Teks dan Tombol */}
-            <div className="flex-1 p-4 md:p-6 lg:p-8 text-center lg:text-left">
+            <div className="flex-1 p-4 md:p-6 lg:p-8 text-center ">
                 <h1 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white mb-4" >
-                    Selamat Datang ke LacakUV
+                    Welcome to LacakUV
                 </h1>
                 <p className="text-base md:text-lg text-gray-600 dark:text-gray-300 mb-6"  >
-                    Tetap vibes di luar tanpa khawatir UV dengan LacakUV! Kami punya panduan UV harian yang simpel dan tips untuk menjaga bumi kita. Semua yang kamu perlu tahu untuk tetap aman dan keren di bawah matahari.
+                Keep the good vibes going outdoors without the UV worries with LacakUV! Our daily UV tips are easy to follow, and we'll show you how to be kind to the planet too. Stay sun-safe and look great while you're at it!
                 </p>
 
                 {/* Tombol Navigasi */}
-                <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start" >
+                <div className="flex flex-col sm:flex-row gap-4 justify-center " >
                     <Link
                         to="/check"
                         className="inline-block px-4 md:px-6 py-3 rounded-lg bg-green-900 dark:bg-[#588157] hover:bg-[#588157] dark:hover:bg-[#A3B18A] text-white font-semibold shadow-md focus:bg-cyan-700 focus:outline-none text-sm md:text-base"
                     >
-                        Check UV kamu!
+                        Check Yours
                     </Link>
                     <a
                         href="#uv-index"
@@ -31,13 +31,7 @@ const LandingPage = () => {
             </div>
 
             {/* Bagian Kiri - Gambar */}
-            <div className="flex-1 flex justify-center items-center mb-8 lg:mb-0" >
-                <img
-                    src="logo.png"
-                    alt="Example Image"
-                    className="w-full max-w-[300px] md:max-w-[400px] lg:max-w-[500px] h-auto object-contain"
-                />
-            </div>
+            
         </section>
     );
 };
